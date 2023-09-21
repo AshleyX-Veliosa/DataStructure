40ac281b9ef2760eb9d4979ac6b73ce579a7e77b
SONTHAYA SUTHOSA <6552300140@stu.pim.ac.th>
2023-09-21 21:40:36 +0700

    Finally i did it Best design (For Me) Every thing should work perfectly

--- CW2/Form1.cs
+++ CW2/Form1.cs
@@ -14,6 +14,7 @@ namespace CW2
     public partial class Form1 : Form
     {
         ArrayList a = new ArrayList(10);
+        int i = 0;
         public Form1()
         {
             InitializeComponent();
@@ -21,7 +22,6 @@ namespace CW2
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -34,6 +34,9 @@ namespace CW2
         private void button1_Click(object sender, EventArgs e)
         {
             a.add(textBox1.Text);
+            i++;
+            textBox3.Text = "" + i;
+            label1.Text = "จำนวน " + i + " คน";
             MessageBox.Show("Added");
             textBox1.Clear();
         }
@@ -42,11 +45,11 @@ namespace CW2
         {
             if (a.contains(textBox1.Text))
             {
-                MessageBox.Show("Found");
+                MessageBox.Show("เจอแล้ว");
             }
             else
             {
-                MessageBox.Show("Noting Found. Did u Add Data?");
+                MessageBox.Show("ไม่เจอใส่ข้อมูลหรือยัง?/");
             }
             textBox1.Clear();
         }
@@ -54,9 +57,73 @@ namespace CW2
         private void button3_Click(object sender, EventArgs e)
         {
             a.remove(textBox1.Text);
-            MessageBox.Show("Found");
-            MessageBox.Show("Removed");
+            if (a.contains(textBox1.Text))
+            {
+                MessageBox.Show("เจอแล้ว");
+                if (textBox3 != null)
+                {
+                    i--;
+                    textBox3.Text = "" + i;
+                }
+                label1.Text = "จำนวน " + i + " คน";
+            }
+            else
+            {
+                MessageBox.Show("ไม่เจอใส่ข้อมูลหรือยัง?/");
+            }
             textBox1.Clear();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int i = int.Parse(textBox2.Text);
+            textBox2.Text = "" + a.get(i - 1);
+        }
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            int i = int.Parse(textBox3.Text);
+            a.set(i - 1, textBox2.Text);
+            MessageBox.Show("แก้ไขเรียบร้อย");
+            label1.Text = "จำนวน " + i + " คน";
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            button4.Enabled = true;
+            button5.Enabled = true;
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            button4.Enabled = true;
+            textBox3.Show();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+                for (int i = 0; i < a.size(); i++)
+                {
+                    richTextBox1.Text += (i+1) + " : " +  a.get(i) + "\n";
+                }
+                if (a.size() == 0)
+                {
+                    MessageBox.Show("ไม่มีข้อมูล");
+                }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
