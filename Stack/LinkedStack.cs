f2471969901ca04ace40c0f94e0c455cbc37b025
SONTHAYA SUTHOSA <sonthaya.q1@gmail.com>
2023-10-02 16:09:20 +0700

    IsCorrectParentheses ,Convert infix to postfix , Calculate postfix expression with Stack Done

---
+++ Stack/LinkedStack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class LinkedStack : Stack
+    {
+        private int SIZE;
+        private LinkedNode first;
+        private class LinkedNode
+        {
+            public object e;
+            public LinkedNode next;
+            public LinkedNode(object e, LinkedNode next)
+            {
+                this.e = e;
+                this.next = next;
+            }
+        }
+        public bool isEmpty()
+        {
+            return SIZE == 0;
+        }
+
+        public object peek()
+        {
+            if (isEmpty())
+                throw new System.MissingMemberException();
+            return first.e;
+        }
+
+        public object pop()
+        {
+            object e = peek();
+            first = first.next;
+            SIZE--;
+            return e;
+        }
+
+        public void push(object e)
+        {
+            first = new LinkedNode(e, first);
+            SIZE++;
+        }
+
+        public int size()
+        {
+            return SIZE;
+        }
+    }
+}
