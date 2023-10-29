37b10b5f9dcf6178009640afd8b8ebba6877292d
SONTHAYA SUTHOSA <6552300140@stu.pim.ac.th>
2023-10-29 11:06:48 +0700

    Rewrite toArray preorder, inorder, and postorder. Export HuffmanTree to array.

---
+++ HuffmanTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using BinaryTree;
+using System;
+
+namespace HuffmanTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] f = { 45, 13, 12, 16, 9, 5 }; // frequencies
+            HuffmanTree t = HuffmanTree.encode(f); //Ajan Bok in slide hai pen toArray
+            Console.WriteLine(t);
+            for (int x = 0; x < t.toArray(2).Length; x++)
+            {
+                Console.WriteLine(t.toArray(2)[x]);
+            }
+        }
+    }
+}
